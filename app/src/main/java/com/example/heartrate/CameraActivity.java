37b7e6f291919930830b51fc704daad67c10054a
Shivanee <Shivaneej02@gmail.com>
2019-03-11 00:31:24 +0530

    Modified interface

--- app/src/main/java/com/example/heartrate/CameraActivity.java
+++ app/src/main/java/com/example/heartrate/CameraActivity.java
@@ -21,6 +21,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity {
     private Camera mCamera;
@@ -29,16 +30,6 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        Button captureButton = findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // get an image from the camera
-                        mCamera.takePicture(null, null, mPicture);
-                    }
-                }
-        );
 
         if(checkCameraHardware(this)){
             // Create an instance of Camera
@@ -95,12 +86,20 @@ public class CameraActivity extends AppCompatActivity {
 
         public void surfaceCreated(SurfaceHolder holder) {
             // The Surface has been created, now tell the camera where to draw the preview.
+            Camera.Parameters p = mCamera.getParameters();
+            List<Camera.Size> sizes  = p.getSupportedPictureSizes();
+            Camera.Size mSize  = null;
+            for(Camera.Size size:sizes)
+            {
+                mSize = size;
+            }
             try {
                 mCamera.setPreviewDisplay(holder);
                 mCamera.startPreview();
             } catch (IOException e) {
                 //Log.d(TAG, "Error setting camera preview: " + e.getMessage());
             }
+            p.setPictureSize(mSize.width,mSize.height);
         }
 
         public void surfaceDestroyed(SurfaceHolder holder) {
@@ -136,69 +135,6 @@ public class CameraActivity extends AppCompatActivity {
                 //Log.d(TAG, "Error starting camera preview: " + e.getMessage());
             }
         }
-    }
-    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
-
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-            if (pictureFile == null){
-                //Log.d(TAG, "Error creating media file, check storage permissions");
-                return;
-            }
-
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                //Log.d(TAG, "File not found: " + e.getMessage());
-            } catch (IOException e) {
-                //Log.d(TAG, "Error accessing file: " + e.getMessage());
-            }
-        }
-    };
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
-
-    /** Create a file Uri for saving an image or video */
-    private static Uri getOutputMediaFileUri(int type){
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    /** Create a File for saving an image or video */
-    private static File getOutputMediaFile(int type){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                Log.d("MyCameraApp", "failed to create directory");
-                return null;
-            }
-        }
-
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE){
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_"+ timeStamp + ".jpg");
-        } else if(type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "VID_"+ timeStamp + ".mp4");
-        } else {
-            return null;
-        }
-
-        return mediaFile;
     }
 
 
